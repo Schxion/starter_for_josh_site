95eabd2c1ea6e01e7138b88427f6649ce3d200c7
Schxion <seanchionh0@gmail.com>
2024-03-17 21:32:45 +0800

    changes for php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="container-fluid text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <a class="navbar-brand calibri" href="#"><span class="josh calibri white">JOSH</span> <span
+                        class="orange calibri">WHITKIN</span></a>
+            </div>
+            <div class="col-md-6">
+                <a class="t-and-c" href="#">TERMS AND CONDITIONS</a>
+            </div>
+        </div> <!-- row -->
+    </div> <!-- container -->
+</footer> <!-- container=fluid -->
+<?php wp_footer(); ?>
+</html>
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+<section class="container-fluid aboutbg text-center">
+    <div class="container">
+        <h2>GAMES &amp; LEVEL DESIGNER</h2>
+        <p class="particle">... and particle wizard</p>
+        <p class="about-text">My name is josh Whitkin. I’m a professional with more than 7 years of industry experience
+            in
+            Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games
+            is the combination of technology and creativity, and how multiple disciplines come together to
+            create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D
+            environments.
+            I always tell a story.</p>
+        <div class="blocker"></div>
+    </div> <!-- container -->
+</section> <!-- container-fluid -->
+
+<section class="container-fluid articlesbg">
+    <div class="container">
+        <h3 class="text-center">LATEST ARTICLES</h3>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src=" http://170.187.231.66/~mesh8/wp-content/themes/starter_for_josh_site/images/portrait1.jpg"
+                    alt="A digital painting of huntress with wild curly hair crouching with a bow in hand in front of a hazy background.">
+                <h4 class="article-title">A PORTRAIT</h4>
+                <p class="date">Friday,November 10. 2030</p>
+                <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS
+                    s
+                    cholars are starting to explore and intervene in the arts. This object of study brings
+                    new challenges and opportunities that we want to explore in this session. We would like
+                    to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                </p>
+                <a class="read-more" href="#">CONTINUE READING</a>
+            </div>
+            <div class="col-md-4">
+                <<img class="img-fluid" src=" http://170.187.231.66/~mesh8/wp-content/themes/starter_for_josh_site/images/portrait2.jpg"
+                    alt="A multi image showcase of a 3D-modeled suit of scifi armour from various angles.">
+                <h4 class="article-title">A PORTRAIT</h4>
+                <p class="date">Friday,November 10. 2030</p>
+                <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS
+                    s
+                    cholars are starting to explore and intervene in the arts. This object of study brings
+                    new challenges and opportunities that we want to explore in this session. We would like
+                    to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                </p>
+                <a class="read-more" href="#">CONTINUE READING</a>
+            </div>
+            <div class="col-md-4">
+            <<img class="img-fluid" src=" http://170.187.231.66/~mesh8/wp-content/themes/starter_for_josh_site/images/portrait3.jpg"
+                    alt="A top down view of a dirt racing circuit surrounded by grass.">
+                <h4 class="article-title">A PORTRAIT</h4>
+                <p class="date">Friday,November 10. 2030</p>
+                <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS
+                    s
+                    cholars are starting to explore and intervene in the arts. This object of study brings
+                    new challenges and opportunities that we want to explore in this session. We would like
+                    to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                </p>
+                <a class="read-more" href="#">CONTINUE READING</a>
+            </div>
+
+        </div> <!-- row -->
+    </div> <!-- container -->
+</section> <!-- container-fluid -->
+
+<?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
